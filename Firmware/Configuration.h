e087e30abd50c94f7dc9c70c428f64a73d3b2f0f
Isaac Feemster <44687816+3DGoat@users.noreply.github.com>
2019-03-24 13:30:48 -0500

    Add files via upload

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -608,7 +608,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 400, 96 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 80, 400, 96 }
 
 /**
  * Default Max Feed Rate (mm/s)
